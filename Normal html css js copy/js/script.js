8cf793898926fef300eb14c12ed1fe58cf8baa01
M S Prince <135182786+MSPrince@users.noreply.github.com>
2024-03-21 00:33:20 +0530

    update code

---
+++ Normal html css js copy/js/script.js
@@ -0,0 +1,43 @@
+// date picker
+$(document).ready(function() {
+    $('#dateRangePicker').daterangepicker({
+      "opens": "left",
+      "locale": {
+        "format": "YYYY-MM-DD"
+      }
+    });
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//------------------------------------------------ for hide code 
+
+// document.addEventListener("contextmenu", (e) => {
+//     e.preventDefault();
+//    }, false);
+  
+//    document.addEventListener("keydown", (e) => {
+//     if (e.ctrlKey || e.keyCode==123) {
+//      e.stopPropagation();
+//      e.preventDefault();
+//     }
+//    });
